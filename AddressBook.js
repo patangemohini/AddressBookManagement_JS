85ad0d095c81e3c00b40b1a622de73f2b02dee7f
mohini patange <patangemohini5@gmail.com>
2022-07-14 19:59:42 +0530

    Added : valid contacts are added using regex

--- AddressBook.js
+++ AddressBook.js
@@ -1,23 +1,36 @@
 console.log("Welcome to address book program !!!!!!");
 
-class Contact {
+const firstNameRegex = RegExp('^[A-Z][a-z]{2,}$');
+const lastNameRegex = RegExp('^[A-Z][a-z]{2,}$');
+const addressRegex = RegExp( '^[a-zA-z]{4,}$' ) ;
+const  cityStateRegex  =  RegExp( '^[a-zA-z]{4,}$' ) ;
+const zipRegex = RegExp('^[0-9]{3}\\s{0,1}[0-9]{3}$');
+const phoneNumberRegex = RegExp('^(91)\\s[0-9]{10}$');
+const emailRegex = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$');
 
-    constructor(...params){
-        this.firstName = params[0];
-        this.lastName = params[1];
-        this.city = params[2];
-        this.state = params[3];
-        this.zip = params[4];
-        this.phoneNumber = params[5];
-        this.email = params[6]
+class Contact{
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
-    get firstName(){
+    get  firstName ( ) {
         return this._firstName;
     }
 
     get lastName(){
         return this._lastName;
+    }
+
+    get address(){
+        return this._address;
     }
 
     get city(){
@@ -41,37 +54,65 @@ class Contact {
     }
 
     set firstName(firstName){
-        this._firstName = firstName;
+        if (firstNameRegex.test(firstName))
+            this._firstName = firstName;
+        else
+            throw " first name is Incorrect ";
     }
 
     set lastName(lastName){
-        this._lastName = lastName;
+        if  ( lastNameRegex . test ( lastName ) )
+            this._lastName = lastName;
+        else
+            throw " last name is Incorrect ";
+    }
+
+    set address(address){
+        if (addressRegex.test(address))
+            this._address = address;
+        else
+            throw " Address is Incorrect ";   
     }
 
     set city(city){
-        this._city = city;
+        if (cityStateRegex.test(city))
+            this._city = city;
+        else
+            throw "**** CITY is Incorrect ****";
     }
 
-    set state(state){
-        this._state = state;
+    set  state ( state ) {
+        if (cityStateRegex.test(state))
+            this._state = state;
+        else
+            throw " state is Incorrect ";
     }
 
     set zip(zip){
-        this._zip = zip;
+        if (zipRegex.test(zip))
+            this._zip = zip;
+        else
+            throw " zip is Incorrect ";
     }
 
     set phoneNumber(phoneNumber){
-        this._phoneNumber = phoneNumber;
+        if (phoneNumberRegex.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else
+            throw " phone number is Incorrect ";
     }
 
     set email(email){
-        this._email = email;
+        if (emailRegex.test(email))
+            this._email = email;
+        else
+            throw "email is incorrect";
     }
 
     toString(){
-        return "First Name : "+ this.firstName + ", Last Name : "+ this.lastName + ", City : "+ this.city + ", State : "+ this.state +", Zip : "+ this.zip+ ", Phone Number : "+ this.phoneNumber + ", Email : "+ this.email;
+        return "First Name : "+ this.firstName + ", Last Name : "+ this.lastName + ", Address : " + this.address + ", City : "+ this.city + ", State : "+ this.state +", Zip : "+ this.zip+ ", Phone Number : "+ this.phoneNumber + ", Email : "+ this.email;
     }
 }
 
-let contact = new Contact("Mohini", "Patange", "latur", "Maharastra", 413512, 9362145144, "Mohini@gmail.com");
-console.log(contact.toString);
+    let contact = new Contact("Mohini", "Patange", "Latur", "Latur", "Maharastra", "413512", "91 9359492123", "Mohini@gmail.com");
+    console.log(contact.toString());
